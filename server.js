856b8e903c13c7d2adce3c6ed4cb3b9e891bc9fa
“malcolmmutare” <mutarem@gmail.com>
2023-07-16 18:41:15 +0200

    server routing init

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res)=> {
+    res.send('this is working');
+});
+
+app.post('/signin', (req, res) => {
+    res.json('singin')
+});
+
 app.listen(3000, ()=> {
-    console.log('app is running on port 3000')
+    console.log('app is running on port 3000');
 })
+
+/*
+/ --> res = this is working
+/signing --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+
+*/
