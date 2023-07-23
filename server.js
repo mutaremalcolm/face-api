0f3145b5f6428f7896826038ceeae6f717b9d11b
“malcolmmutare” <mutarem@gmail.com>
2023-07-23 15:26:52 +0200

    reverted server import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
-import register from './controllers/register'
+//import register from './controllers/register.js'
 
  const db = knex({
     client: 'pg',
@@ -49,7 +49,35 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'));
 });
 
-app.post('/register', (req, res)=> { register.handleRegister(req, res, db, bcrypt)})
+app.post('/register', (req, res)=> { 
+    const {email, name, password} = req.body; 
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx =>{
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users') 
+            .returning('*')
+            .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            }).then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    
+    .catch(err => res.status(400).json('unable to register'))
+}
+);
+    //register.handleRegister(req, res, db, bcrypt)})
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params; 
