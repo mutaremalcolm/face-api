5e43a4a5b4b9aaaa645102c30109a0a52af83f78
“malcolmmutare” <mutarem@gmail.com>
2023-07-24 12:10:28 +0200

    completed modules

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
 import register from './controllers/register.cjs';
-
-
+import signin from './controllers/signin.cjs';
+import profile from './controllers/profile.cjs';
+import image from './controllers/image.cjs';
 
 
  const db = knex({
@@ -32,74 +33,11 @@ app.get('/', (req, res)=> {
     res.send('success');
 });
 
-app.post('/signin', (req, res) => { 
-    const { email, password} = req.body;
-    if (!email || !password) {
-        return res.status(400).json('incorrect form submission');
-    }
-    db.select('email', 'hash').from('login')
-    .where('email', '=', email)
-    .then(data => {
-        const isValid = bcrypt.compareSync(password, data[0].hash); 
-        if (isValid) {
-            return db.select('*').from('users')
-            .where('email', '=', email)
-            .then(user => {
-            res.json(user[0])
-            })
-            .catch(err => res.status(400).json('unable to get user'));   
-        }else {
-            res.status(400).json('wrong credentials');
-        }
-        
-    })   
-    .catch(err => res.status(400).json('wrong credentials'));
-});
-
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
-
-    //register.handleRegister(req, res, db, bcrypt)})
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params; 
-    db.select('*').from('users').where({
-        id: id
-    }).then(user => {
-        if (user.length) {
-            res.json(user[0]);
-        }else {
-            res.status(400).json('Not found');
-        }
-        
-    })
-    .catch(err => res.status(400).json('error getting user'))     
-})
-
-
-
-app.put('/image', (req, res)=> {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries);
-    })  
-    .catch(err => res.status(400).json('unable to get entries'))      
-})
+app.get('/profile/:id', (req, res)=> {profile.handleProfileGet(req, res, db)});
+app.put('/image', (req, res)=> {image.handleImage(req, res, db)});
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
 })
-
-
-
-/*
-/ --> res = this is working
-/signing --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-
-
-*/
