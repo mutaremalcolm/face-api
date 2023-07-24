68db92f105b80f5fbfec5eeab1b15106915262e8
“malcolmmutare” <mutarem@gmail.com>
2023-07-24 11:29:35 +0200

    fixed module bug

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
+import register from './controllers/register.cjs';
 
 
 
@@ -55,37 +56,8 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'));
 });
 
-app.post('/register', (req, res)=> { 
-    const {email, name, password} = req.body; 
-    if (!email || !name || !password) {
-        return res.status(400).json('incorrect form submission');
-    }
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx =>{
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users') 
-            .returning('*')
-            .insert({
-                email: loginEmail[0].email,
-                name: name,
-                joined: new Date()
-            }).then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    
-    .catch(err => res.status(400).json('unable to register'))
-}
-);
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+
     //register.handleRegister(req, res, db, bcrypt)})
 
 app.get('/profile/:id', (req, res) => {
